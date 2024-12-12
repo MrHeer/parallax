70f333782834b3cd49c5d01daa6bddebb91cdcb8
He Linming <hlm52pk@163.com>
2024-12-12 16:33:53 +0800

    chore: remove unused import

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:parallax/import_image_dialog.dart';
 import 'package:parallax/store.dart';
 import 'package:parallax/parallax_image.dart';
